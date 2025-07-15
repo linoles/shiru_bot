6f1e7cb92e319231cbeb6d633d425b29dc04eeb2
linoles <cernikovdenis081@gmail.com>
2025-07-16 01:46:11 +0300

    update

--- src/app/pages/api/getAllUsers/route.ts
+++ src/app/pages/api/getAllUsers/route.ts
@@ -1,21 +1,6 @@
 // src/app/api/getAllUsers/route.ts
 import { NextResponse } from 'next/server';
-import db from '@/src/app/db';
 
-export async function GET() {
-  try {
-    const users = await new Promise((resolve, reject) => {
-      db.all("SELECT tgId, tgNick, tgUsername FROM users_data", (error, result) => {
-        if (error) reject(error);
-        resolve(result || []);
-      });
-    });
-
-    return NextResponse.json(users);
-  } catch (error) {
-    return NextResponse.json(
-      { error: error },
-      { status: 500 }
-    );
-  }
+export async function POST() {
+  return NextResponse.json([{ tgId: 1, tgNick: "test", tgUsername: "test" }]);
 }
