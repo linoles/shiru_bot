3e8d085edc1f0c045edae2ba97fd56871e1aa2d9
linoles <cernikovdenis081@gmail.com>
2025-07-15 17:17:24 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import "@/app/globals.css";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {   
+    console.log(navigator);
+  }, []);
   return (
     <div id="root">
-      {/*hk*/ }
       <div role="region" aria-label="Notifications (F8)" tabIndex={-1} style={{ pointerEvents: "none" }}>
         <ol tabIndex={-1} className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]"></ol>
       </div>
