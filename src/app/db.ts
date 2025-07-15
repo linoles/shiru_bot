1b6ccd9043e1a593d2e999c127d953e59b9733e6
linoles <cernikovdenis081@gmail.com>
2025-07-16 01:56:20 +0300

    update

--- src/app/db.ts
+++ src/app/db.ts
@@ -8,12 +8,13 @@ const __dirname = dirname(__filename);
 
 const dbPath = path.resolve(__dirname, "users.db");
 
-let db = new sqlite3.Database(dbPath, (err) => {
+let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
   if (err) {
-    console.error(err.message);
+    console.error("SQLITE_CANTOPEN: unable to open database file");
   } else {
     console.log("Connected to the SQLite database.");
   }
 });
 
 export default db;
+
