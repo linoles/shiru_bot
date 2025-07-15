24f2e94fd3bdeeff1eb6631b52f24dc4a9020c76
linoles <cernikovdenis081@gmail.com>
2025-07-16 02:08:14 +0300

    update

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,23 +1,11 @@
 import sqlite3 from "sqlite3";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const dbPath = path.resolve(__dirname, "users.db");
-
-let db = new sqlite3.Database(
-  dbPath,
-  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
-  (err) => {
-    if (err) {
-      console.error(err.message);
-    } else {
-      console.log("Connected to the SQLite database.");
-    }
+let db = new sqlite3.Database("src/app/users.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log("Connected to the SQLite database.");
   }
-);
+});
 
 export default db;
