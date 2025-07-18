a2cebb3f55671cb1a5984338a1e95769ae4ea9cb
linoles <cernikovdenis081@gmail.com>
2025-07-18 18:15:35 +0300

    update

--- src/app/casino/users-client.tsx
+++ src/app/casino/users-client.tsx
@@ -111,13 +111,17 @@ export default function ClientComponent({ initialUsers }: { initialUsers: User[]
         // mb.showProgress(true);
         mb.onClick(() => location.href = "/casino/free");
         mb.show();
+        const sb = tg.SecondaryButton;
+        sb.enable();
+        sb.setParams({ text: "Крутить ($100 очков)" });
+        sb.onClick(rollSlots);
+        sb.show();
       }
     } catch (error) {
       console.error(error)
     }
   }, [])
 
-  const tg = window.Telegram?.WebApp;
   useEffect(() => {
     if (!tgData?.id) return
 
@@ -144,67 +148,10 @@ export default function ClientComponent({ initialUsers }: { initialUsers: User[]
           setCurUser(foundUser);
         }
       }
-        const sb = tg.SecondaryButton;
-        sb.enable();
-        sb.setParams({ text: "Крутить ($100 очков)" });
-        sb.onClick(async () => {
-          try {
-            if (curUser.points < 100) {
-              alert("Недостаточно очков!");
-              return;
-            }
-            const newRandChoices = [symbols[Math.floor(Math.random() * 4)], symbols[Math.floor(Math.random() * 4)], symbols[Math.floor(Math.random() * 4)]];
-            setInt(newRandChoices.map((choice, index) => (<div key={index} className={`h-[30vw] w-full bg-card rounded-3xl text-5xl flex items-center justify-center ${choice}`}>{choice}</div>)))
-            const payoutKey = newRandChoices.join('')
-            const payout = payouts[payoutKey] || 0
-            if (payout <= 16) {
-              setRes("x0 (-100$)");
-              setResColor("red");
-              curUser.points -= 100;
-            } else if (payout >= 17 && payout < 32) {
-              setRes(`x0.5 (-50$)`);
-              setResColor("red");
-              curUser.points -= 50;
-            } else if (payout == 32) {
-              setRes("x1 (=0$)");
-              setResColor("stone");
-            } else if (payout >= 33 && payout <= 48) {
-              setRes("x1.5 (+50$)");
-              setResColor("green");
-              curUser.points += 50;
-            } else if (payout >= 49 && payout < 64) {
-              setRes("x2 (+100$)");
-              setResColor("green");
-              curUser.points += 100;
-            } else {
-              setRes("х3 (+200$)");
-              setResColor("green");
-              curUser.points += 200;
-            }
-            const response = await fetch('/api/save-user', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(curUser),
-            })
-
-            if (!response.ok) {
-              const errorData = await response.json()
-              throw new Error(errorData.error || "Ошибка сервера")
-            }
-
-            const result = await response.json()
-            console.log("Успешно:", result)
-          } catch (e) {
-            console.error(e);
-          }
-        });
-        sb.show();
     }
 
     checkAndAddUser()
-  }, [tgData, users, tg])
+  }, [tgData, users])
 
   const rollSlots = async () => {
     try {
