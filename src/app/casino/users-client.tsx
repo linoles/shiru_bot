04109ac6ad9039fa6d1047b6e4f8c5d7b74f8c47
linoles <cernikovdenis081@gmail.com>
2025-07-19 01:06:03 +0300

    update

--- src/app/casino/users-client.tsx
+++ src/app/casino/users-client.tsx
@@ -119,7 +119,7 @@ export default function ClientComponent({ initialUsers }: { initialUsers: User[]
             setResColor("stone");
             sb.showProgress(true);
             setInt(rand_choices.map((choice, index) => (<div key={index} className={`h-[30vw] w-full bg-card rounded-3xl text-5xl flex items-center justify-center ${choice}`}><img src={`${choice}.svg`} className="w-[60%]"></img></div>)))
-            sb.showProgress(false);
+            setTimeout(() => sb.showProgress(false), 2000);
             await new Promise(resolve => setTimeout(resolve, 2000));
             const newRandChoices = [symbols[Math.floor(Math.random() * 4)], symbols[Math.floor(Math.random() * 4)], symbols[Math.floor(Math.random() * 4)]];
             setInt(newRandChoices.map((choice, index) => (<div key={index} className={`h-[30vw] w-full bg-card rounded-3xl text-5xl flex items-center justify-center ${choice}`}><img src={`${choice}.svg`} className="w-[60%]"></img></div>)))
