61586fbceda49e976c6e75a0e9bf5805414aa833
linoles <cernikovdenis081@gmail.com>
2025-07-20 21:54:24 +0300

    update

--- src/app/casino/free/users-client.tsx
+++ src/app/casino/free/users-client.tsx
@@ -380,7 +380,7 @@ export default function ClientComponent({ initialUsers }: { initialUsers: User[]
             <span className="text-sm text-muted-foreground ml-2 py-3 px-2 bg-inherit min-w-[80vw] text-start max-w-[80vw] border-b border-border">FIRST PLACE | USERNAME | POINTS</span>
           </div>
           <div className="flex items-center justify-center w-full h-12 mt-2">
-            <span className="text-sm text-muted-foreground ml-2 py-3 px-2 bg-inherit min-w-[80vw] text-start max-w-[80vw] border-b border-border">SECOND PLACE | USERNAME | POINTS</span>
+            <span className="text-sm text-muted-foreground ml-2 py-3 px-2 bg-inherit min-w-[80vw] text-end max-w-[80vw] border-b border-border">SECOND PLACE | USERNAME | POINTS</span>
             <div className="flex items-center justify-center w-12 h-12 bg-orange rounded-full min-w-[48px]">
               <Image src="/2.png" alt="copy" className="w-6 h-6" width={24} height={24} />
             </div>
